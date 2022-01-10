002bc732d2dea7bc41e0a1e4b5821720c685191a
tanyugang <tanyugang.com@gmail.com>
2022-01-10 12:42:00 +0800

    Code for UEFI-17

--- Boot.c
+++ Boot.c
@@ -71,47 +71,13 @@ UefiMain(
     BOOT_CONFIG BootConfig;
     BootConfig.VideoConfig = VideoConfig;
     BootConfig.AsciiBmp = BmpConfig;
-
-
-    
+ 
     UINT64 (*KernelEntry)(BOOT_CONFIG *BootConfig);
     KernelEntry = (UINT64 (*)(BOOT_CONFIG *BootConfig))KernelEntryPoint;
-    // = {4096, NULL, 4096, 0, 0, 0};
-    BootConfig.MemoryMap.BufferSize = 4096;
-    BootConfig.MemoryMap.Buffer = NULL;
-    BootConfig.MemoryMap.MapSize = 4096;
-    BootConfig.MemoryMap.MapKey = 0;
-    BootConfig.MemoryMap.DescriptorSize = 0;
-    BootConfig.MemoryMap.DescriptorVersion = 0;
-    Status = ByeBootServices(ImageHandle, &BootConfig.MemoryMap);
+  
     UINT64 PassBack = KernelEntry(&BootConfig);
     Print(L"PassBack=%d.\n", PassBack);
     //Never return here
-    return Status;
-}
-
-EFI_STATUS ByeBootServices(EFI_HANDLE ImageHandle, OUT MEMORY_MAP *MemoryMap)
-{
-    EFI_STATUS Status = EFI_SUCCESS;
-    
-    Status = gBS->AllocatePool(EfiLoaderData, MemoryMap->BufferSize, &MemoryMap->Buffer);
-    if(EFI_ERROR(Status)){
-        Print(L"Failed to allocate memory to get memory map.\n");
-        return Status;
-    }
-
-    Status = gBS->GetMemoryMap(
-                &MemoryMap->MapSize,
-                (EFI_MEMORY_DESCRIPTOR*)MemoryMap->Buffer,
-                &MemoryMap->MapKey,
-                &MemoryMap->DescriptorSize,
-                &MemoryMap->DescriptorVersion);
-
-    Status = gBS->ExitBootServices(ImageHandle, MemoryMap->MapKey);
-    if(EFI_ERROR(Status)){
-        Print(L"Could not exit boot services : %r.\n",Status);
-    }
-
     return Status;
 }
 
