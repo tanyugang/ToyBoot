c5d7bf71664d90dcf1d00cfec4da3c475658ae0c
tanyugang <tanyugang.com@gmail.com>
2022-01-10 12:40:03 +0800

    Code for UEFI-16

--- Boot.h
+++ Boot.h
@@ -2,35 +2,11 @@
 #include "Elf.h"
 
 #pragma pack(1)
-typedef struct
-{
-    UINTN BufferSize;
-    VOID *Buffer;
-    UINTN MapSize;
-    UINTN MapKey;
-    UINTN DescriptorSize;
-    UINT32 DescriptorVersion;
-} MEMORY_MAP;
 
 typedef struct
 {
     VIDEO_CONFIG VideoConfig;
-    MEMORY_MAP   MemoryMap;
     BMP_CONFIG AsciiBmp;
 } BOOT_CONFIG; // @Boot.h
 
-typedef struct
-{
-    UINT16 LimitLow;
-    UINT16 BaseLow;
-    UINT8  BaseMiddle;
-    UINT8  SysFlag;
-    UINT8  LimitHigh;
-    UINT8  BaseHigh;
-} GDT_SELECTOR;
-
-EFI_STATUS ByeBootServices(
-    IN EFI_HANDLE ImageHandle,
-    OUT MEMORY_MAP *MemoryMap
-);
 #pragma pack()
